f9e24567d0d4e37ea4d8b88b75bf8db1ab1e7a9e
manavok <mitmanav6805@gmail.com>
2025-06-24 19:01:03 +0530

    Implement custom rendering logic and update main entry point for Custom React app

--- viteReact/src/App.jsx
+++ viteReact/src/App.jsx
@@ -1,10 +1,10 @@
 import Greet from "./greet";
 function App() {
-
+  const username = 'Manav'
   return (
     <>
       <Greet/>
-      <h2>Hare Ram</h2>
+      <h2>Hare Ram {username}</h2>
     </>
     
   );
