5577a5f158cd1b8b2ee92087ece6e394938c9cbf
manavok <mitmanav6805@gmail.com>
2025-10-02 12:24:41 +0530

    Starting to learn react

--- basics/src/Greet.jsx
+++
@@ -1,6 +0,0 @@
-function Greet(){
-    return (
-        <h2>Hare Ram Hare Ram</h2>
-    )
-}
-export default Greet
--- viteReact/src/greet.jsx
+++
@@ -1,6 +0,0 @@
-function Greet(){
-    return (
-        <h2>Hare krsna</h2>
-    )
-}
-export default Greet
