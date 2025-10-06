f3710ed4ac7f5b9de0506e97b5a89240da88c1ed
manavok <mitmanav6805@gmail.com>
2025-10-06 13:45:52 +0530

    Learn react router and implement navigation bar

---
+++ capstone_project/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,21 @@
+import { Fragment } from "react"
+import { Link, Outlet } from "react-router"
+import CrownLogo from '../../assets/crown.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => (
+  <Fragment>
+    <nav className="navigation">
+        <Link className="logo-container" to='/'>
+            <img className="logo" src={CrownLogo} alt="Crown Logo" />
+        </Link>
+        <div className="nav-links-container">
+            {/* <Link className="nav-link" to="/">Home</Link> | */}
+            <Link className="nav-link" to="/shop">SHOP</Link> 
+            {/* <Link to="/market">Market</Link> */}
+        </div>
+    </nav>
+    <Outlet/>
+  </Fragment>
+)
+export default Navigation
